51493d181839574d6dd05d84fab83709cc3dd2b7
Roberto Garcia <rgarcia2893@gmail.com>
2025-05-27 11:59:54 +0200

    modèle ingrédients codé - reste à faire : dependencies

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,2 +1,37 @@
 class Ingredient < ApplicationRecord
+  UNIT_TYPES = [
+  "g",
+  "l",
+  "cup",
+  "tbsp",
+  "tsp",
+  "piece"
+]
+
+FOOD_TYPES = [
+  "vegetable",
+  "fruit",
+  "meat",
+  "fish",
+  "dairy",
+  "grain",
+  "spice",
+  "condiment",
+  "beverage",
+  "sweet",
+  "frozen",
+  "prepared",
+  "other"
+]
+
+STORAGE_TYPES = [
+  "dry",
+  "fridge",
+  "freezer",
+]
+  validates :name, presence: true
+  validates :quantity, presence: true
+  validates :unit_type, presence: true, inclusion: { in: UNIT_TYPES }
+  validates :food_type, presence: true, inclusion: { in: FOOD_TYPES }
+  validates :storage_type, presence: true, inclusion: { in: STORAGE_TYPES}
 end
