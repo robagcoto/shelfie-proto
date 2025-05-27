4267e6fdc1c2bad4776963ed6f2fc4133acb7829
Fabien Hoarau <hoarauf4@gmail.com>
2025-05-27 12:36:00 +0200

    add has_many

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :ingredients
+  has_many :recipes
+
+  validates :email, presence: true, uniqueness: true
+  validates :encrypted_password, presence: true
 end
