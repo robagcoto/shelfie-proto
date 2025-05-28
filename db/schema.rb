3f5f398d6a08b81c9ece206062aecb2facbc3bea
victortaib <victortaib96@gmail.com>
2025-05-28 15:14:18 +0200

    ringredients recipe

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.1].define(version: 2025_05_28_123102) do
-
+ActiveRecord::Schema[7.1].define(version: 2025_05_28_131215) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -99,11 +97,9 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_28_123102) do
     t.string "name"
     t.string "description"
     t.integer "rating"
-    t.string "ingredients"
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "quantity"
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_recipes_on_user_id"
   end
