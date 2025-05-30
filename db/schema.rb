92f3fee1cb76b745b7e5785a35998df97f36718e
christophe chea <christophe.chea@gmail.com>
2025-05-30 14:53:31 +0200

    Update form, delete button, edit button

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_29_131754) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_30_121838) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -111,9 +111,10 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_29_131754) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
-    t.boolean "favorite", default: false
     t.string "duration"
     t.text "steps"
+    t.boolean "favorite", default: false
+    t.integer "number_of_ingredients"
     t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
