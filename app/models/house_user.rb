5a9d3615452de5208c3eb08bae5396f12b009abd
Roberto Garcia <rgarcia2893@gmail.com>
2025-06-02 17:25:04 +0200

    création modèles

---
+++ app/models/house_user.rb
@@ -0,0 +1,4 @@
+class HouseUser < ApplicationRecord
+  belongs_to :house
+  belongs_to :user
+end
---
+++ test/models/house_ingredient_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class HouseIngredientTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
