1f28b4ecfb3912d051b43fa3299d14b152837492
GautierDavid <gautier.d.91@gmail.com>
2025-06-05 21:19:30 +0200

    remove useless code

--- app/javascript/controllers/ingredients_controller.js
+++ app/javascript/controllers/ingredients_controller.js
@@ -25,18 +25,19 @@ export default class extends Controller {
         <input type="text" name="ingredient_name${this.count}" id="ingredient_name" placeholder="Ingredients" class="form-control">
       </div>
 
-      <div class="col-4">
+      <div class="col-3">
         <input type="text" name="ingredient_quantity${this.count}" id="ingredient_quantity" placeholder="Quantity" class="form-control">
       </div>
 
-      <div class="col-2">
-        <select name="ingredient_unit${this.count}" id="ingredient_unit" class="form-select"><option value="l">l</option>
+      <div class="col-3">
+        <select name="ingredient_unit${this.count}" id="ingredient_unit" class="form-select">
           <option value="g">g</option>
+          <option value="l">l</option>
           <option value="pc(s)">pc(s)</option>
         </select>
       </div>
 
-        <div class="col-2 p-0 d-flex justify-content-center">
+        <div class="col-1 ms-auto p-0 d-flex justify-content-center">
           <button data-ingredients-target="button" data-action="click->ingredients#addIngredients" type="button" class="btn btn-primary">+</button>
         </div>
     </div>`
