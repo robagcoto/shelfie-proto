9911134ca5dfacd6a879d8d4129c24087f1fe719
Roberto Garcia <rgarcia2893@gmail.com>
2025-06-06 15:01:22 +0200

    création de HouseIngredient pour un Ingrédient existant

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,39 +1,28 @@
 class Ingredient < ApplicationRecord
-#   UNIT_TYPES = [
-#   "g",
-#   "l",
-#   "cup",
-#   "tbsp",
-#   "tsp",
-#   "piece"
-# ]
+  CATEGORIES = [
+      "Fruits",
+      "Vegetables",
+      "Bread, cereals, and nuts",
+      "Meats",
+      "Fish and seafood",
+      "Dairy and eggs",
+      "Legumes",
+      "Beverages",
+      "Sweets",
+      "Processed foods and ready meals",
+  ]
 
-CATEGORIES = [
-    "Fruits",
-    "Vegetables",
-    "Bread, cereals, and nuts",
-    "Meats",
-    "Fish and seafood",
-    "Dairy and eggs",
-    "Legumes",
-    "Beverages",
-    "Sweets",
-    "Processed foods and ready meals",
-]
+  STORAGE_METHODS = [
+    "Dry",
+    "Fridge",
+    "Freezer"
+  ]
 
-STORAGE_METHODS = [
-  "Dry",
-  "Fridge",
-  "Freezer"
-]
+  has_many :house_ingredients
+  validates :storage_method, presence: true, inclusion: { in: STORAGE_METHODS }
+  validates :category, presence: true, inclusion: { in: CATEGORIES }
 
-#   validates :name, presence: true
-#   validates :quantity, presence: true
-#   validates :unit_type, presence: true, inclusion: { in: UNIT_TYPES }
-#   validates :food_type, presence: true, inclusion: { in: FOOD_TYPES }
-
-has_many :house_ingredients
-validates :storage_method, presence: true, inclusion: { in: STORAGE_METHODS }
-validates :category, presence: true, inclusion: { in: CATEGORIES }
-
+  def self.normalized_name(name)
+    name.to_s.strip.downcase.singularize
+  end
 end
