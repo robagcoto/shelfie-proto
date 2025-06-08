ae7581cc625b9877748f124d9173ad05ccede45c
Fabien Hoarau <hoarauf4@gmail.com>
2025-06-08 19:42:46 +0200

    update show/edit house ingredient. add models house ingredient and history. add house_ingredients_controllers. 2 migrations column house_ingredient and table histories

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,15 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "@popperjs/core"
 import "bootstrap"
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+document.addEventListener("turbo:load", () => {
+  flatpickr(".flatpickr", {
+    altInput: true,
+    altFormat: "F j, Y",
+    dateFormat: "Y-m-d",
+    allowInput: false,
+    clickOpens: true
+  });
+});
