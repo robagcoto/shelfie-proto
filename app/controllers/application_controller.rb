d92d0cce23d46ac644ed5ae21996710b8dd93319
Roberto Garcia <rgarcia2893@gmail.com>
2025-06-12 15:53:26 +0200

    config og

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,8 @@ class ApplicationController < ActionController::Base
   def set_default_house
     @house = current_user.houses.first if user_signed_in?
   end
+
+def default_url_options
+  { host: ENV["DOMAIN"] || "localhost:3000" }
+end
 end
